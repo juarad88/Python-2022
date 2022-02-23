3a046eb563a49d892d5ad47661f1606492ad3044
juarad88 <100236884+juarad88@users.noreply.github.com>
2022-02-24 10:59:33 +1300

    practice code

---
+++ practice_code.py
@@ -0,0 +1,4 @@
+print("Dre")
+print("Mrs White")
+print("Kings' High school")
+print(7*4/8*23)
--- test.py
+++ test.py
@@ -1,2 +1,10 @@
 print("hello world")
 print("joe")
+command = input("Do you want to play a game? [y or n]")
+print("")
+if command == "y":
+	print("No you don't")
+elif command == "n":
+	print("Yes you do")
+else:
+	print("Please enter valid answer. y or n.")

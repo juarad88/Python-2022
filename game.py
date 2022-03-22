13a60e204080d2e27da682690062b9e790f8eaa2
juarad88 <100236884+juarad88@users.noreply.github.com>
2022-03-23 10:56:28 +1300

    my game intro part 1

---
+++ game.py
@@ -0,0 +1,13 @@
+from adventurelib import *
+#All new code goes here
+
+
+
+
+#starts the game
+def main():
+	print("You are Desmond, the great hero of the west, and you are a paladin. After returning after defeating the dreadful beast in the Cave Of Sorrows, you hear of a wicked wizard in Alastair and decide to investigate. Upon your arrival,  you discover a red-clad wizard. He addresses you and tells you to leave. You respond by informing him that he is the one who must depart. With broadsword in hand, racing up to the wizard, as your blade slices through him, a plume of crimson smoke clouds your vision , leaving you unconscious.")
+	start()
+
+if __name__ == '__main__':
+	main() 
